083f618da65dc9df14c77ae96b213bb9773eed3f
Kseniya <chekan.ksenia@gmail.com>
2022-01-21 21:33:43 +0200

    js/script.js add first task

--- js/script.js
+++ js/script.js
@@ -30,14 +30,39 @@
 
 // 9. ОПЕРАТОРЫ JS
 
-console.log('arr' + ' - object');
-console.log(4 + +'5');
+// console.log('arr' + ' - object');
+// console.log(4 + +'5');
 
-let incr = 10,
-decr = 10;
+// let incr = 10,
+// decr = 10;
 
-// ++incr;
-// decr--;
+// // ++incr;
+// // decr--;
 
-console.log(++incr);
-console.log(--decr);
+// console.log(++incr);
+// console.log(--decr);
+
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
+
+
+
+
